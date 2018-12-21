d65365272d013a144e3cf3094c6ead826921bc3a
Cedric Chee <cedric@invictusbyte.com>
2018-12-21 22:30:57 +0800

    CSS and Styled Components

--- frontend/components/Page.js
+++ frontend/components/Page.js
@@ -1,6 +1,16 @@
 import React, { Component } from 'react';
 import Header from './Header';
 import Meta from './Meta';
+import styled from 'styled-components';
+
+const MyButton = styled.button`
+  background: darkgreen;
+  font-size: ${props => (props.huge ? '100px' : '50px')};
+
+  .poop {
+    font-size: 50px;
+  }
+`;
 
 class Page extends Component {
   render() {
@@ -8,6 +18,15 @@ class Page extends Component {
       <div>
         <Meta />
         <Header />
+        <MyButton huge>
+          Click Me
+          <span className="poop">😅</span>
+        </MyButton>
+        {this.props.children}
+        <MyButton>
+          Click Me
+          <span className="poop">😅</span>
+        </MyButton>
         {this.props.children}
       </div>
     );
