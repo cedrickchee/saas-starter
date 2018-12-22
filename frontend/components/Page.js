77e46ea6357660a1eb41988aa22f13d3339e6bb9
Cedric Chee <cedric@invictusbyte.com>
2018-12-23 00:14:00 +0800

    Global styling and typography with Styled Components

--- frontend/components/Page.js
+++ frontend/components/Page.js
@@ -24,6 +24,33 @@ const Inner = styled.div`
   padding: 2rem;
 `;
 
+injectGlobal`
+  @font-face {
+    font-family: 'radnika_next';
+    src: url('/static/radnika-medium-webfont.woff2') format('woff2');
+    font-weight: normal;
+    font-style: normal;
+  }
+  html {
+    box-sizing: border-box;
+    font-size: 10px;
+  }
+  *, *:before, *:after {
+    box-sizing: inherit;
+  }
+  body {
+    padding: 0;
+    margin: 0;
+    font-size: 1.5rem;
+    line-height: 2;
+    font-family: 'radnika_next';
+  }
+  a {
+    text-decoration: none;
+    color: ${theme.black};
+  }
+`;
+
 class Page extends Component {
   render() {
     return (
