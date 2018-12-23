079e3dd8d7dbcf54e4b537536eb44c6d4345f1c7
Cedric Chee <cedric@invictusbyte.com>
2018-12-23 10:27:18 +0800

    Fix font file 404

--- frontend/components/Page.js
+++ frontend/components/Page.js
@@ -27,7 +27,7 @@ const Inner = styled.div`
 injectGlobal`
   @font-face {
     font-family: 'radnika_next';
-    src: url('/static/radnika-medium-webfont.woff2') format('woff2');
+    src: url('/static/radnikanext-medium-webfont.woff2') format('woff2');
     font-weight: normal;
     font-style: normal;
   }
