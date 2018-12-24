9dc3b0bd2e5ae0f298e921031689cc863e30bc8d
Cedric Chee <cedric@invictusbyte.com>
2018-12-24 18:49:43 +0800

    Implement GraphQL Query and Mutation resolvers, Prisma Yoga flow

--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -1,8 +1,12 @@
+const { forwardTo } = require('prisma-binding');
+
 const Query = {
-  dogs(parent, args, ctx, info) {
-    global.dogs = global.dogs || [];
-    return global.dogs;
-  },
+  items: forwardTo('db'),
+  // async items(parent, args, ctx, info) {
+  //   console.log('Getting items');
+  //   const items = await ctx.db.query.items();
+  //   return items;
+  // },
 };
 
 module.exports = Query;
