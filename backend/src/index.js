1b7a05abdb2ce2ad3deef289439695b220111eb1
Cedric Chee <cedric@invictusbyte.com>
2019-01-14 12:16:31 +0800

    Fix backend CORS issue

--- backend/src/index.js
+++ backend/src/index.js
@@ -9,8 +9,10 @@ const server = createServer();
 
 server.start(
   {
-    cors: true,
-    origin: process.env.FRONTEND_URL,
+    cors: {
+      credentials: true,
+      origin: process.env.FRONTEND_URL,
+    },
   },
   deets => {
     console.log(`Server is now running on port http://localhost:${deets.port}`);
