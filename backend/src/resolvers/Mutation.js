0c26ee54c75b660151231c3b2253ac1da8ce508f
Cedric Chee <cedric@invictusbyte.com>
2019-01-15 20:15:49 +0800

    Update shop items with Queries and Mutations

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -11,16 +11,26 @@ const Mutations = {
       info
     );
 
+    console.log(item);
+
     return item;
   },
-
-  // createDog(parent, args, ctx, info) {
-  //   global.dogs = global.dogs || [];
-  //   // create a dog
-  //   const newDog = { name: args.name };
-  //   global.dogs.push(newDog);
-  //   return newDog;
-  // },
+  updateItem(parent, args, ctx, info) {
+    // first take a copy of the updates
+    const updates = { ...args };
+    // remove the ID from the updates
+    delete updates.id;
+    // run the update method
+    return ctx.db.mutation.updateItem(
+      {
+        data: updates,
+        where: {
+          id: args.id,
+        },
+      },
+      info
+    );
+  },
 };
 
 module.exports = Mutations;
--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -2,6 +2,7 @@ const { forwardTo } = require('prisma-binding');
 
 const Query = {
   items: forwardTo('db'),
+  item: forwardTo('db'),
   // async items(parent, args, ctx, info) {
   //   console.log('Getting items');
   //   const items = await ctx.db.query.items();
