493160c6ffa4dbda72112f3c3c8e936e711a8a6b
Cedric Chee <cedric@invictusbyte.com>
2019-01-16 14:07:54 +0800

    Backend pagination

--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -3,6 +3,7 @@ const { forwardTo } = require('prisma-binding');
 const Query = {
   items: forwardTo('db'),
   item: forwardTo('db'),
+  itemsConnection: forwardTo('db'),
   // async items(parent, args, ctx, info) {
   //   console.log('Getting items');
   //   const items = await ctx.db.query.items();
