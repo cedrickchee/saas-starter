15bb2c51b01267dedd115e7d903ba0e0756307a4
Cedric Chee <cedric@invictusbyte.com>
2019-04-17 17:38:38 +0800

    Frontend reset password flow

--- frontend/pages/signup.js
+++ frontend/pages/signup.js
@@ -1,5 +1,6 @@
 import Signup from '../components/Signup';
 import Signin from '../components/Signin';
+import RequestReset from '../components/RequestReset';
 import styled from 'styled-components';
 
 const Columns = styled.div`
@@ -12,6 +13,7 @@ const SignupPage = props => (
   <Columns>
     <Signup />
     <Signin />
+    <RequestReset />
   </Columns>
 );
 
