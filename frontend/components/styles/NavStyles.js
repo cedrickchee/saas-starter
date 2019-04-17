fbd47e4f8e86e473f3865f53f67663b94ec68a5e
Cedric Chee <cedric@invictusbyte.com>
2019-04-17 13:11:12 +0800

    Fix Sign Out button style

--- frontend/components/styles/NavStyles.js
+++ frontend/components/styles/NavStyles.js
@@ -19,7 +19,7 @@ const NavStyles = styled.ul`
     border: 0;
     cursor: pointer;
     color: ${props => props.theme.black};
-    font-weight: 800;
+    /* font-weight: 800; */
     @media (max-width: 700px) {
       font-size: 10px;
       padding: 0 10px;
