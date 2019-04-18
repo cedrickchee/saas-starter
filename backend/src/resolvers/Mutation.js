6dd12e836a913e8bb5ad7ad456b0d10d78310825
Cedric Chee <cedric@invictusbyte.com>
2019-04-18 20:28:25 +0800

    Item and User data relationships

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -7,11 +7,19 @@ const { transport, makeANiceEmail } = require('../mail');
 
 const Mutations = {
   async createItem(parent, args, ctx, info) {
-    // TODO: check if they are logged in
+    if (!ctx.request.userId) {
+      throw new Error('You must be logged in to do that!');
+    }
 
     const item = await ctx.db.mutation.createItem(
       {
         data: {
+          // This is how to create a relationship between the Item and the User
+          user: {
+            connect: {
+              id: ctx.request.userId,
+            },
+          },
           ...args,
         },
       },
