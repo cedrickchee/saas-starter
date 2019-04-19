36cb8f621cf380b744340c480e7984c4e55dceaa
Cedric Chee <cedric@invictusbyte.com>
2019-04-19 10:02:51 +0800

    Remove console.log

--- frontend/components/PleaseSignIn.js
+++ frontend/components/PleaseSignIn.js
@@ -5,7 +5,6 @@ import Signin from './Signin';
 const PleaseSignIn = props => (
   <Query query={CURRENT_USER_QUERY}>
     {({ data, loading }) => {
-      console.log(data);
       if (loading) <p>Loading...</p>;
       if (!data.me) {
         return (
