599b2c04d0f5e574a6c37c1ed2009bd2903f3589
Cedric Chee <cedric@invictusbyte.com>
2019-04-20 18:57:30 +0800

    Cache updates with Apollo & optimistic UI

--- frontend/components/RemoveFromCart.js
+++ frontend/components/RemoveFromCart.js
@@ -27,11 +27,38 @@ class RemoveFromCart extends React.Component {
   static propTypes = {
     id: PropTypes.string.isRequired
   };
+  // This gets called as soon as we get a response back from the server after a mutation has been performed
+  update = (cache, payload) => {
+    // The cache is the Apollo cache
+    // The payload is the actual dump of information that is returned from the server
+    console.log('Running remove from cart update fn');
+    // 1. First read the cache
+    const data = cache.readQuery({
+      query: CURRENT_USER_QUERY
+    });
+    console.log(data);
+    // 2. Remove that item from the cart
+    // payload.data is what we are getting back from the server
+    // inside of that we get removeFromCart Mutation
+    // and inside of that we have an id
+    const cartItemId = payload.data.removeFromCart.id;
+    data.me.cart = data.me.cart.filter(cartItem => cartItem.id !== cartItemId);
+    // 3. Write it back to the cache
+    cache.writeQuery({ query: CURRENT_USER_QUERY, data });
+  };
   render() {
     return (
       <Mutation
         mutation={REMOVE_FROM_CART_MUTATION}
         variables={{ id: this.props.id }}
+        update={this.update}
+        optimisticResponse={{
+          __typename: 'Mutation',
+          removeFromCart: {
+            __typename: 'CartItem',
+            id: this.props.id
+          }
+        }}
       >
         {(removeFromCart, { loading, error }) => (
           <BigButton
