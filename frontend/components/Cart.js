df28a59d0c3b49524f87782e0334632bac569c55
Cedric Chee <cedric@invictusbyte.com>
2019-04-20 20:00:41 +0800

    Remove console.log

--- frontend/components/Cart.js
+++ frontend/components/Cart.js
@@ -26,7 +26,6 @@ const Cart = () => (
   <User>
     {({ data: { me } }) => {
       if (!me) return null;
-      console.log(me);
       return (
         <Mutation mutation={TOGGLE_CART_MUTATION}>
           {toggleCart => (
