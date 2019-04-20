a638c1b04cf953f2ad36bf0c75d23bc2d8f3fd40
Cedric Chee <cedric@invictusbyte.com>
2019-04-20 16:45:10 +0800

    Disable 'Add to Cart' button as it's doing the work

--- frontend/components/AddToCart.js
+++ frontend/components/AddToCart.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Mutation } from 'react-apollo';
 import gql from 'graphql-tag';
+import { CURRENT_USER_QUERY } from './User';
 
 const ADD_TO_CART_MUTATION = gql`
   mutation addToCart($id: ID!) {
@@ -20,8 +21,13 @@ class AddToCart extends React.Component {
         variables={{
           id
         }}
+        refetchQueries={[{ query: CURRENT_USER_QUERY }]}
       >
-        {addToCart => <button onClick={addToCart}>Add To Cart</button>}
+        {(addToCart, { loading }) => (
+          <button disabled={loading} onClick={addToCart}>
+            Add{loading && 'ing'} To Cart
+          </button>
+        )}
       </Mutation>
     );
   }
