7c59ab33d025d0dfab7f9ce459cbd83b6d5e833e
Cedric Chee <cedric@invictusbyte.com>
2019-04-21 02:01:14 +0800

    Process credit card with Stripe Checkout

--- frontend/components/Cart.js
+++ frontend/components/Cart.js
@@ -10,6 +10,7 @@ import SickButton from './styles/SickButton';
 import CartItem from './CartItem';
 import calcTotalPrice from '../lib/calcTotalPrice';
 import formatMoney from '../lib/formatMoney';
+import TakeMyMoney from './TakeMyMoney';
 
 const LOCAL_STATE_QUERY = gql`
   query {
@@ -57,7 +58,9 @@ const Cart = () => (
           </ul>
           <footer>
             <p>{formatMoney(calcTotalPrice(me.cart))}</p>
-            <SickButton>Checkout</SickButton>
+            <TakeMyMoney>
+              <SickButton>Checkout</SickButton>
+            </TakeMyMoney>
           </footer>
         </CartStyles>
       );
