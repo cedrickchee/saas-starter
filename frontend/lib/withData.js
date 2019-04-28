67acb5c31bc5d5182cf5aae7299db92b6c11af83
Cedric Chee <cedric@invictusbyte.com>
2019-04-28 19:23:51 +0800

    Production endpoint for Yoga GraphQL server

--- frontend/lib/withData.js
+++ frontend/lib/withData.js
@@ -1,11 +1,11 @@
 import withApollo from 'next-with-apollo';
 import ApolloClient from 'apollo-boost';
-import { endpoint } from '../config';
+import { endpoint, prodEndpoint } from '../config';
 import { LOCAL_STATE_QUERY } from '../components/Cart';
 
 function createClient({ headers }) {
   return new ApolloClient({
-    uri: process.env.NODE_ENV === 'development' ? endpoint : endpoint,
+    uri: process.env.NODE_ENV === 'development' ? endpoint : prodEndpoint,
     request: operation => {
       operation.setContext({
         fetchOptions: {
